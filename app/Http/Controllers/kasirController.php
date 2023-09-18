ed42f161ad16a1d759312947a28b16148970f327
timotius089 <timotius089@gmail.com>
2023-09-18 11:27:55 +0700

    kasir_update_kurir_ditugaskan

--- app/Http/Controllers/kasirController.php
+++ app/Http/Controllers/kasirController.php
@@ -4,28 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class kasirController extends Controller
 {
     public function login_kasir(Request $request) {
-        $users = DB::table('users')->where('jabatan', 'kurir')->get();
+        $users = DB::table('users')->where('jabatan', 'kurir')->where('status', '')->get();
         return view('kasir/kasir',['users' => $users]);
     }
     
     public function tugaskan(Request $request) {
         // dd($request->all());
         $id = $request->id;
-        $jenis_jasa = $request->jenis_jasa;
-        $harga_perkg = $request->harga_perkg;
-
 		$data = array(
-			'jenis_jasa' =>$jenis_jasa,
-			'harga_perkg' =>$harga_perkg,
+			'status' =>'Ditugaskan',
 		);
-        // $ProdukJasa = ProdukJasa::find($id);
-        // $ProdukJasa->update($data);
+        $user = user::find($id);
+        $user->update($data);
      
-        return response()->json($data);
+        return response()->json($id);
     }
 }
