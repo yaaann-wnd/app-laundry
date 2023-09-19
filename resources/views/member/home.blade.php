53aa8f3272b27bbcb07bd3423a2fd205065e9edc
Aftiyan <aftiyangamer45@gmail.com>
2023-09-19 14:20:45 +0700

    tambah produk dummy di halaman home

--- resources/views/member/home.blade.php
+++ resources/views/member/home.blade.php
@@ -1,69 +1,121 @@
 @extends('layouts.sidebar_member')
 
 @section('content')
-<div class="promo mb-5">
-    <div class="promo-wrapper">
-        <img src="{{ asset('images/gambar-slider/1.jpg') }}" alt="">
-        <div class="teks-promo-wrapper">
-            <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
-            <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo! </span>
-            <div class="tombol-wrapper">
-                <button class="btn btn-primary">Pesan Sekarang</button>
+    <div class="promo">
+        <div class="promo-wrapper">
+            <img src="{{ asset('images/gambar-slider/1.jpg') }}" alt="">
+            <div class="teks-promo-wrapper">
+                <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
+                <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo!
+                </span>
+                <div class="tombol-wrapper">
+                    <button class="btn btn-primary">Pesan Sekarang</button>
+                </div>
+            </div>
+        </div>
+        <div class="promo-wrapper">
+            <img src="{{ asset('images/gambar-slider/2.jpg') }}" alt="">
+            <div class="teks-promo-wrapper">
+                <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
+                <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo!
+                </span>
+                <div class="tombol-wrapper">
+                    <button class="btn btn-primary">Pesan Sekarang</button>
+                </div>
+            </div>
+        </div>
+        <div class="promo-wrapper">
+            <img src="{{ asset('images/gambar-slider/3.jpg') }}" alt="">
+            <div class="teks-promo-wrapper">
+                <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
+                <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo!
+                </span>
+                <div class="tombol-wrapper">
+                    <button class="btn btn-primary">Pesan Sekarang</button>
+                </div>
+            </div>
+        </div>
+        <div class="promo-wrapper">
+            <img src="{{ asset('images/gambar-slider/1.jpg') }}" alt="">
+            <div class="teks-promo-wrapper">
+                <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
+                <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo!
+                </span>
+                <div class="tombol-wrapper">
+                    <button class="btn btn-primary">Pesan Sekarang</button>
+                </div>
+            </div>
+        </div>
+        <div class="promo-wrapper">
+            <img src="{{ asset('images/gambar-slider/2.jpg') }}" alt="">
+            <div class="teks-promo-wrapper">
+                <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
+                <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo!
+                </span>
+                <div class="tombol-wrapper">
+                    <button class="btn btn-primary">Pesan Sekarang</button>
+                </div>
+            </div>
+        </div>
+        <div class="promo-wrapper">
+            <img src="{{ asset('images/gambar-slider/3.jpg') }}" alt="">
+            <div class="teks-promo-wrapper">
+                <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
+                <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo!
+                </span>
+                <div class="tombol-wrapper">
+                    <button class="btn btn-primary">Pesan Sekarang</button>
+                </div>
             </div>
         </div>
     </div>
-    <div class="promo-wrapper">
-        <img src="{{ asset('images/gambar-slider/2.jpg') }}" alt="">
-        <div class="teks-promo-wrapper">
-            <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
-            <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo! </span>
-            <div class="tombol-wrapper">
-                <button class="btn btn-primary">Pesan Sekarang</button>
+    <div class="judul-produk text-center my-5">
+        <h3>Pilih Paket</h3>
+    </div>
+    <div class="produk mb-5">
+        <div class="card">
+            <img src="{{ asset('images/gambar-slider/2.jpg') }}" class="card-img-top" alt="...">
+            <div class="card-body kartu ">
+                <h5 class="card-title">Cuci Kering</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+                <div>
+                    <a href="#" class="btn btn-primary">Pesan</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{ asset('images/gambar-slider/2.jpg') }}" class="card-img-top" alt="...">
+            <div class="card-body kartu">
+                <h5 class="card-title">Cuci Kering + Setrika</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+                <div>
+                    <a href="#" class="btn btn-primary">Pesan</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{ asset('images/gambar-slider/2.jpg') }}" class="card-img-top" alt="...">
+            <div class="card-body kartu">
+                <h5 class="card-title">Cuci Premium + Setrika</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+                <div>
+                    <a href="#" class="btn btn-primary">Pesan</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{ asset('images/gambar-slider/2.jpg') }}" class="card-img-top" alt="...">
+            <div class="card-body kartu">
+                <h5 class="card-title">✨Cuci Paket Aesthetic✨</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+                <div>
+                    <a href="#" class="btn btn-primary">Pesan</a>
+                </div>
             </div>
         </div>
     </div>
-    <div class="promo-wrapper">
-        <img src="{{ asset('images/gambar-slider/3.jpg') }}" alt="">
-        <div class="teks-promo-wrapper">
-            <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
-            <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo! </span>
-            <div class="tombol-wrapper">
-                <button class="btn btn-primary">Pesan Sekarang</button>
-            </div>
-        </div>
-    </div>
-    <div class="promo-wrapper">
-        <img src="{{ asset('images/gambar-slider/1.jpg') }}" alt="">
-        <div class="teks-promo-wrapper">
-            <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
-            <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo! </span>
-            <div class="tombol-wrapper">
-                <button class="btn btn-primary">Pesan Sekarang</button>
-            </div>
-        </div>
-    </div>
-    <div class="promo-wrapper">
-        <img src="{{ asset('images/gambar-slider/2.jpg') }}" alt="">
-        <div class="teks-promo-wrapper">
-            <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
-            <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo! </span>
-            <div class="tombol-wrapper">
-                <button class="btn btn-primary">Pesan Sekarang</button>
-            </div>
-        </div>
-    </div>
-    <div class="promo-wrapper">
-        <img src="{{ asset('images/gambar-slider/3.jpg') }}" alt="">
-        <div class="teks-promo-wrapper">
-            <span class="judul">Pesan 2 kali, Gratis 3 kali!</span>
-            <span class="desc">Tunggu apalagi, ayo buruan pesan di Zea Laundry. This is once in a lifetime promo! </span>
-            <div class="tombol-wrapper">
-                <button class="btn btn-primary">Pesan Sekarang</button>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="produk">
-    
-</div>
 @endsection
