b52979733f6c8641f8cb8cddfd65be38801faaf4
Aftiyan <aftiyangamer45@gmail.com>
2023-09-22 15:50:21 +0700

    hapus badge new

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -309,7 +309,6 @@
                         <a class="nav-link" href="{{ route('harga_jasa') }}">
                             <i class="typcn typcn-device-desktop menu-icon"></i>
                             <span class="menu-title">Dashboard</span>
-                            <div class="badge badge-danger">new</div>
                         </a>
                     </li>
                     <li class="nav-item {{ Auth::user()->jabatan == 'admin' ? '' : 'd-none' }}">
